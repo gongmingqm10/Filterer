8da40ab51c5883f6eefa25156f43f1e385b0a012
mingong <gongmingqm10@foxmail.com>
2016-08-22 23:26:02 +0100

    Add black white and layer filters

--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -10,24 +10,26 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegateFlowLayout ,UICollectionViewDataSource {
 
+    @IBOutlet weak var targetImage: UIImageView!
     @IBOutlet weak var filtersCollectionView: UICollectionView!
     let filtersData = [["label": "Original", "image": "FilterOriginal"],
                        ["label": "Black & White", "image": "FilterOriginal"],
-                       ["label": "Light", "image": "FilterOriginal"],
-                       ["label": "Popular", "image": "FilterOriginal"],
-                       ["label": "Beauty", "image": "FilterOriginal"],
-                       ["label": "Makeup", "image": "FilterOriginal"],
-                       ["label": "Sweet", "image": "FilterOriginal"]
-                       ]
+                       ["label": "Layer", "image": "FilterOriginal"]]
+    let filtersType = [FilterType.ORIGINAL, FilterType.BW, FilterType.LAYER]
+    var originalImage: UIImage?
+    var imageFilters: ImageFilters?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         filtersCollectionView.dataSource = self
         filtersCollectionView.delegate = self
+        originalImage = UIImage(named: "SampleImage")!
+        
+        targetImage.image = originalImage
+        imageFilters = ImageFilters(image: originalImage!)
     }
     
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -47,6 +49,19 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout ,UICo
         filterViewCell.populate(filterData["label"]!, imageName: filterData["image"]!)
     
         return filterViewCell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        var processedImage: UIImage
+        switch filtersType[indexPath.row] {
+        case .BW:
+            processedImage = imageFilters!.apply(BWFilter(intensity: 0.5))
+        case .LAYER:
+            processedImage = imageFilters!.apply(LayerFilter(intensity: 0.5))
+        default:
+            processedImage = originalImage!
+        }
+        targetImage.image = processedImage
     }
 
 
---
+++ Filterer/filters/FilterType.swift
@@ -0,0 +1,15 @@
+//
+//  FilterType.swift
+//  Filterer
+//
+//  Created by Ming Gong on 8/22/16.
+//  Copyright © 2016 Ming Gong. All rights reserved.
+//
+
+import Foundation
+
+enum FilterType {
+    case ORIGINAL
+    case BW
+    case LAYER
+}
